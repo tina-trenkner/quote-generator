d97d612b2bdf15a3ed1ede54eeef3b2c8ae0090b
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2016-08-11 15:32:35 +1200

    added call to Ron Swanson API

--- quote-generator.js
+++ quote-generator.js
@@ -1,1 +1,12 @@
-document.addEventListener()
+var xhr = new XMLHttpRequest();
+xhr.open("GET", 'https://ron-swanson-quotes.herokuapp.com/v2/quotes'; false)
+xhr.send();
+
+console.log(xhr.status):
+console.log(xhr.statusText);
+
+document.getElementsByClass("button").addEventListener("click", myFunction);
+
+var myFunction = function {
+  document.getElementsByClass("quote").innerHTML = /* something from quote API;*/
+}
