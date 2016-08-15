e9115ca22cbaac936d28414ac358d12302bd80ea
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2016-08-15 16:18:54 +1200

    JS now pulling in API responseText

--- quote-generator.js
+++ quote-generator.js
@@ -1,26 +1,10 @@
-var showQuote = function () {
+function showQuote() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/", true);
   xhr.setRequestHeader("X-Mashape-Authorization", "K1FgNiR6aRmshUIfTmKaMn9WxgLgp1PO0oLjsnTKF0VeCym0rK");
+
   xhr.onload = function () {
-        var responseText = xhr.responseText
-         document.getElementById("quote").innerHTML = responseText;
+         document.getElementById("quote").innerHTML = xhr.responseText;
     }
-  xhr.send();
+      xhr.send();
 };
-
-
-
-
-
-
-
-
-
-
-
-
-/* code cribbed from http://codepen.io/roessland/pen/bedjGB */
-
-
-/* document.getElementById("button").addEventListener("click", getQuote);
