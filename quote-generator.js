4c43f3ea324fb63f1a146b8e7760b1c12c7d30fd
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2016-08-28 20:32:17 +1200

    added code to parse string

--- quote-generator.js
+++ quote-generator.js
@@ -1,10 +1,17 @@
 function showQuote() {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/", true);
+  xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies", true);
   xhr.setRequestHeader("X-Mashape-Authorization", "K1FgNiR6aRmshUIfTmKaMn9WxgLgp1PO0oLjsnTKF0VeCym0rK");
-
+  xhr.send();
   xhr.onload = function () {
-         document.getElementById("quote").innerHTML = xhr.responseText;
-    }
-      xhr.send();
-};
+      var data = xhr.responseText;
+      var quoteOnly = JSON.parse(data);
+      document.getElementById("quote").innerHTML = quoteOnly.quote;
+      document.getElementById("movie").innerHTML = quoteOnly.author; 
+    };
+ };
+
+
+/* var justTheQuote = new Obj () {
+  xhr.responseText.
+} */
